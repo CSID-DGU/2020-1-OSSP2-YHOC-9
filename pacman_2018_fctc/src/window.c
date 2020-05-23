b76968254ae91e2b83b4b77fa0d0636bc4f5d265
Yoonho Kim <maumnamu@gmail.com>
2020-05-23 23:04:38 +0900

    resizing

--- pacman_2018_fctc/src/window.c
+++ pacman_2018_fctc/src/window.c
@@ -1,10 +1,13 @@
 #include "window.h"
+#include <SDL/SDL_rotozoom.h>
 
 #define SCREEN_WID 448
 #define SCREEN_HEI 640
 
-static SDL_Surface *screen;
-static SDL_ResizeEvent event_w;
+SDL_Surface *screen;
+SDL_Surface *scr_temp;//화면에 출력을 하기 전에 저장을 위한 s
+SDL_Rect scr_rec;
+SDL_Rect scr_temp_rec;
 
 bool init_window(const char* title, int width, int height)
 {
@@ -13,8 +16,20 @@ bool init_window(const char* title, int width, int height)
 		return false;
 	}
 
-	screen = SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE | SDL_RESIZABLE);
+	screen = SDL_SetVideoMode(width, height, 32, SDL_SWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE);
+	scr_temp = SDL_CreateRGVSurface(SDL_SWSURFACE, width, height, screen->format->BitsPerPixel,
+								screen->format->Rmask, screen->format->Gmask,
+								screen->format->Bmask, screen->format->Amask);
 
+	scr_rec.x = 0;
+	scr_rec.y = 0;
+	scr_rec.w = width;
+	scr_rec.h = height;
+
+	scr_temp_rec.x = 0;
+	scr_temp_rec.y = 0;
+	scr_temp_rec.w = width;
+	scr_temp_rec.h = height;
 	if (screen == NULL)
 	{
 		return false;
@@ -32,7 +47,7 @@ void dispose_window(void)
 
 SDL_Surface *get_screen(void)
 {
-	return screen;
+	return scr_temp;
 }
 
 void clear_screen(int r, int g, int b, int a)
@@ -45,21 +60,20 @@ void clear_screen(int r, int g, int b, int a)
 void apply_surface(int x, int y, SDL_Surface* source)
 {
 	SDL_Rect offset;
-	if(event_w.w != 0 || event_w.h != 0)
-	{
-		offset.x = (event_w.w / SCREEN_WID) * x;
-		offset.y = (event_w.h / SCREEN_HEI) * y;
-	}
-	else
-	{
-		offset.x = x;
-		offset.y = y;
-	}
+	offset.x = x;
+	offset.y = y;
 	
-	SDL_BlitSurface(source, NULL, screen, &offset);
+	SDL_BlitSurface(source, NULL, scr_temp, &offset);
 }
 
 void flip_screen(void)
 {
+	SDL_Surface * temp;
+	double zoom_w = (double) scr_temp_rec.w/SCREEN_WID;
+	double zoom_h = (double) scr_temp_rec.w/SCREEN_WID;
+
+	SDL_BlitSurface(temp, &scr_temp_rec, scr_temp, &scr_temp_rec);
+
 	SDL_Flip(screen);
+	SDL_FreeSurface(temp);
 }
