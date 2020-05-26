5de13ed4c797e86a41d9a7920150848678f50e33
yoonho kim <maumnamu2@naver.com>
2020-05-26 11:23:02 +0900

    add comments

--- pacman_2018_fctc/src/window.c
+++ pacman_2018_fctc/src/window.c
@@ -12,6 +12,7 @@ bool init_window(const char* title, int width, int height)
 	}
 
 	screen = SDL_SetVideoMode(SCREEN_WID, SCREEN_HEI, 32, SDL_SWSURFACE | SDL_RESIZABLE);
+	//resizable 플래그를 추가를 해서 resize가 가능하게 만들었습니다. 
 
 	if (screen == NULL)
 	{
@@ -55,11 +56,12 @@ void flip_screen(void)
 
 	double zoom_w = (double) scr_temp_rec.w /SCREEN_WID;
 	double zoom_h = (double) scr_temp_rec.h /SCREEN_HEI;
+	//크기 비율이 얼만큼 변했는지를 알기 위한 계산
 	
 	temp = zoomSurface(screen, zoom_w, zoom_h, 0);
-
+	//rotozoom을 사용을 해서 비율을 조절한 것을 temp에 저장
 	SDL_BlitSurface(temp, NULL, screen, &scr_temp_rec);
-
+	//temp를 screen에 적용
 	SDL_FreeSurface(temp);
 	
 	SDL_Flip(screen);
