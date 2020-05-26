90df60da1de556e26fb1c5493b37e64fd41a1e61
yoonho kim <maumnamu2@naver.com>
2020-05-26 11:24:05 +0900

    add comments

--- pacman_2018_fctc/src/window.h
+++ pacman_2018_fctc/src/window.h
@@ -6,6 +6,7 @@
 
 SDL_Surface *screen;
 SDL_Rect scr_temp_rec;
+//화면 구성 요소
 
 //Initializes the window to the given length, width and title.
 bool init_window(const char* title, int width, int height);
